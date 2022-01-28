59c74a0a2ffedc8716c0c069e6d618cf018e7a39
Tilda Källström <tildakallstrom@gmail.com>
2022-01-28 21:15:04 +0100

    first commit

---
+++ js/js.js
@@ -0,0 +1,91 @@
+// function för att ändra "läs mer" till "dölj" 
+function myFunction() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "+ Läs mer";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "- Dölj";
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunctionOne() {
+    let dotsone = document.getElementById("dotsone");
+    let moreTextone = document.getElementById("moreone");
+    let btnTextone = document.getElementById("myBtnone");
+  
+    if (dotsone.style.display === "none") {
+      dotsone.style.display = "inline";
+      btnTextone.innerHTML = "+ Läs mer";
+      moreTextone.style.display = "none";
+    } else {
+      dotsone.style.display = "none";
+      btnTextone.innerHTML = "- Dölj";
+      moreTextone.style.display = "inline";
+    }
+  }
+  function myFunctionTwo() {
+    let dotstwo = document.getElementById("dotstwo");
+    let moreTexttwo = document.getElementById("moretwo");
+    let btnTexttwo = document.getElementById("myBtntwo");
+  
+    if (dotstwo.style.display === "none") {
+      dotstwo.style.display = "inline";
+      btnTexttwo.innerHTML = "+ Läs mer";
+      moreTexttwo.style.display = "none";
+    } else {
+      dotstwo.style.display = "none";
+      btnTexttwo.innerHTML = "- Dölj";
+      moreTexttwo.style.display = "inline";
+    }
+  }
+  function myFunctionThree() {
+    let dotsthree = document.getElementById("dotsthree");
+    let moreTextthree = document.getElementById("morethree");
+    let btnTextthree = document.getElementById("myBtnthree");
+  
+    if (dotsthree.style.display === "none") {
+      dotsthree.style.display = "inline";
+      btnTextthree.innerHTML = "+ Läs mer";
+      moreTextthree.style.display = "none";
+    } else {
+      dotsthree.style.display = "none";
+      btnTextthree.innerHTML = "- Dölj";
+      moreTextthree.style.display = "inline";
+    }
+  }
+  function myFunctionFour() {
+    let dotsfour = document.getElementById("dotsfour");
+    let moreTextfour = document.getElementById("morefour");
+    let btnTextfour = document.getElementById("myBtnfour");
+  
+    if (dotsfour.style.display === "none") {
+      dotsfour.style.display = "inline";
+      btnTextfour.innerHTML = "+ Läs mer";
+      moreTextfour.style.display = "none";
+    } else {
+      dotsfour.style.display = "none";
+      btnTextfour.innerHTML = "- Dölj";
+      moreTextfour.style.display = "inline";
+    }
+  }
+  function myFunctionFive() {
+    let dotsfive = document.getElementById("dotsfive");
+    let moreTextfive = document.getElementById("morefive");
+    let btnTextfive = document.getElementById("myBtnfive");
+  
+    if (dotsfive.style.display === "none") {
+      dotsfive.style.display = "inline";
+      btnTextfive.innerHTML = "+ Läs mer";
+      moreTextfive.style.display = "none";
+    } else {
+      dotsfive.style.display = "none";
+      btnTextfive.innerHTML = "- Dölj";
+      moreTextfive.style.display = "inline";
+    }
+  }
